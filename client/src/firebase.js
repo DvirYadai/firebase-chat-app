72fdeb32feaad46636d0cc0a2cf5b43d8a6c2db8
DvirYadai <74467515+DvirYadai@users.noreply.github.com>
2021-05-16 15:28:00 +0300

    update

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -2,12 +2,12 @@ import firebase from "firebase/app";
 import "firebase/auth";
 
 const app = firebase.initializeApp({
-  apiKey: "AIzaSyAg_AzmxM3B4-ysVqjGXWuxQe9xWa-pwzY",
-  authDomain: "chat-app-90456.firebaseapp.com",
-  projectId: "chat-app-90456",
-  storageBucket: "chat-app-90456.appspot.com",
-  messagingSenderId: "953953202969",
-  appId: "1:953953202969:web:657b9fd48a28f973958b88",
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
 });
 
 export const auth = app.auth();
